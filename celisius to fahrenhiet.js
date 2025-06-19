7c1472d56ead1e375af915fd296692cbadaf53eb
chandinidevi06 <eegala.chandini2004@gmail.com>
2025-06-19 09:29:13 +0530

    add the new file

--- celisius to fahrenhiet.js
+++ celisius to fahrenhiet.js
@@ -1,11 +1,11 @@
-var celsius =100
-var fahrenhiet=(celsius*1.8)+32;
+var celsius = 100
+var fahrenhiet = (celsius*1.8) + 32;
 console.log(`${celsius}degree celsius is equal to ${fahrenhiet}degree fahrenhiet`);
 let temp =25;
 if(temp > 30){
     console.log("it's hot");
 }else if(temp>20){
-    console.log("nice weather!");
+    console.log("nice weather");
 }else{
     console.log("it's cold!");
 }
--- swapping of 2 no.js
+++ swapping of 2 no.js
@@ -4,8 +4,7 @@ var b=(20);
     temp = a;
     a=b;
     b=temp;
-}
+
 console.log(`the value of a after swapping:${a}`);
 console.log(`the value of a after swapping:${b}`);
-
-
+}
